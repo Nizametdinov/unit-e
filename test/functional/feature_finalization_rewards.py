cdc0ba5eff098c55deb349376351cadbaafcf97b
Azat Nizametdinov <azat@thirdhash.com>
2019-05-22 18:01:12 +0200

    Functional test for finalization reward fraction
    Signed-off-by: Azat Nizametdinov <azat@thirdhash.com>

--- test/functional/feature_finalization_rewards.py
+++ test/functional/feature_finalization_rewards.py
@@ -41,6 +41,14 @@ class FinalizationRewardsTest(UnitETestFramework):
             block = node.getblock(block_hash)
             return node.getrawtransaction(block['tx'][0], True, block_hash)
 
+        def check_finalization_rewards(block, reward_size, reward_addresses):
+            for out in epoch_first_cb['vout'][1:EPOCH_LENGTH + 1]:
+                assert_equal(out['value'], reward_size)
+
+            addresses = sum((x['scriptPubKey']['addresses']
+                             for x in epoch_first_cb['vout'][1:EPOCH_LENGTH + 1]), [])
+            assert_equal(addresses, reward_addresses)
+
         node = self.nodes[0]
         finalizer1, finalizer2 = self.nodes[1:]
 
@@ -54,7 +62,7 @@ class FinalizationRewardsTest(UnitETestFramework):
         proposer_address1 = node.getnewaddress('', 'bech32')
         proposer_address2 = node.getnewaddress('', 'bech32')
 
-        node.generatetoaddress(1, proposer_address1)
+        node.proposetoaddress(1, proposer_address1)
         assert_equal(node.getblockcount(), 1)
         epoch_first_cb = get_coinbase_of_last_block(node)
         # The first block of the first epoch does not have finalization rewards
@@ -65,48 +73,76 @@ class FinalizationRewardsTest(UnitETestFramework):
         sync_blocks([node, finalizer1, finalizer2])
 
         payto = finalizer1.getnewaddress('', 'legacy')
-        txid = finalizer1.deposit(payto, 5000)
+        txid = finalizer1.deposit(payto, 3000)
         wait_until(lambda: txid in node.getrawmempool())
         payto = finalizer2.getnewaddress('', 'legacy')
-        txid = finalizer2.deposit(payto, 5000)
+        txid = finalizer2.deposit(payto, 6000)
         wait_until(lambda: txid in node.getrawmempool())
 
         disconnect_nodes(node, finalizer1.index)
         disconnect_nodes(node, finalizer2.index)
 
-        reward_addresses = [proposer_address1]
-        for _ in range(EPOCH_LENGTH - 3):
-            node.generatetoaddress(1, proposer_address1)
-            reward_addresses.append(proposer_address1)
-            assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS)
+        assert_equal(node.getblockcount(), 1)
 
-        for _ in range(2):
-            node.generatetoaddress(1, proposer_address2)
-            reward_addresses.append(proposer_address2)
-            assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS)
+        for _ in range(3):
+            reward_addresses = [proposer_address1]
+            for _ in range(EPOCH_LENGTH - 3):
+                node.proposetoaddress(1, proposer_address1)
+                reward_addresses.append(proposer_address1)
+                assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS)
 
-        node.generatetoaddress(1, proposer_address1)
+            for _ in range(2):
+                node.proposetoaddress(1, proposer_address2)
+                reward_addresses.append(proposer_address2)
+                assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS)
 
-        # The first block of the second epoch must have finalization rewards for each block of the previous epoch
+            node.proposetoaddress(1, proposer_address1)
+
+            # The first block of each epoch must have finalization rewards for each block of the previous epoch
+            epoch_first_cb = get_coinbase_of_last_block(node)
+            assert_equal(len(epoch_first_cb['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+            check_finalization_rewards(epoch_first_cb, FULL_FINALIZATION_REWARD, reward_addresses)
+
+        assert_equal(node.getblockcount(), EPOCH_LENGTH * 3 + 1)
+        print(node.getfinalizationstate())
+
+        # No votes included
+        node.proposetoaddress(EPOCH_LENGTH, proposer_address2)
+        reward_addresses = [proposer_address1] + [proposer_address2] * (EPOCH_LENGTH - 1)
         epoch_first_cb = get_coinbase_of_last_block(node)
         assert_equal(len(epoch_first_cb['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+        check_finalization_rewards(epoch_first_cb, FULL_FINALIZATION_REWARD, reward_addresses)
+        print(node.getfinalizationstate())
 
-        for out in epoch_first_cb['vout'][1:EPOCH_LENGTH + 1]:
-            assert_equal(out['value'], FULL_FINALIZATION_REWARD)
+        # Vote of the firs validator included
+        self.wait_for_vote_and_disconnect(finalizer=finalizer1, node=node)
+        node.proposetoaddress(EPOCH_LENGTH, proposer_address1)
+        reward_addresses = [proposer_address2] + [proposer_address1] * (EPOCH_LENGTH - 1)
+        epoch_first_cb = get_coinbase_of_last_block(node)
+        assert_equal(len(epoch_first_cb['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+        check_finalization_rewards(epoch_first_cb, FULL_FINALIZATION_REWARD, reward_addresses)
+        print(node.getfinalizationstate())
 
-        addresses = sum((x['scriptPubKey']['addresses']
-                         for x in epoch_first_cb['vout'][1:EPOCH_LENGTH + 1]), [])
-        assert_equal(addresses, reward_addresses)
+        # Vote of the second validator included
+        self.wait_for_vote_and_disconnect(finalizer=finalizer2, node=node)
+        node.proposetoaddress(EPOCH_LENGTH, proposer_address2)
+        reward_addresses = [proposer_address1] + [proposer_address2] * (EPOCH_LENGTH - 1)
+        epoch_first_cb = get_coinbase_of_last_block(node)
+        assert_equal(len(epoch_first_cb['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+        check_finalization_rewards(epoch_first_cb, FULL_FINALIZATION_REWARD, reward_addresses)
+        print(node.getfinalizationstate())
 
-        for _ in range(EPOCH_LENGTH - 1):
-            node.generatetoaddress(1, proposer_address2)
-            reward_addresses.append(proposer_address2)
-            assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS)
-        node.generatetoaddress(1, proposer_address1)
-        assert_equal(len(get_coinbase_of_last_block(node)['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+        # All the votes are included
+        self.wait_for_vote_and_disconnect(finalizer=finalizer1, node=node)
+        self.wait_for_vote_and_disconnect(finalizer=finalizer2, node=node)
+        node.proposetoaddress(EPOCH_LENGTH, proposer_address1)
+        reward_addresses = [proposer_address2] + [proposer_address1] * (EPOCH_LENGTH - 1)
+        epoch_first_cb = get_coinbase_of_last_block(node)
+        assert_equal(len(epoch_first_cb['vout']), CB_DEFAULT_OUTPUTS + EPOCH_LENGTH)
+        check_finalization_rewards(epoch_first_cb, FULL_FINALIZATION_REWARD, reward_addresses)
+        print(node.getfinalizationstate())
 
         # Check that rewards are not spendable until the corresponding epoch is finalized
-        # Check that rewards are proportional to the number of votes included in the epoch
 
 
 if __name__ == '__main__':
